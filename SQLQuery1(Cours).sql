3cf60c19e2aa550446e18a62dc0c4614dac10839
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-12-27 23:54:38 +0530

    Initial Commit

---
+++ SQLQuery1(Cours).sql
@@ -0,0 +1,9 @@
+create database MVC_Assignment13Db
+use MVC_Assignment13Db
+create table Courses
+(CId int primary key,
+CName nvarchar(50) not null,
+CFee float not null,
+Technology nvarchar(50) not null,
+Status nvarchar(50) not null)
+
